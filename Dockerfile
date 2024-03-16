cd7fc35e35c128492f364e6a2e226ef04578044d
litencatt <ncl0709@gmail.com>
2024-03-17 00:28:03 +0900

    Init dev env

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+# Specifies a parent image
+FROM golang:1.22-bullseye
+
+WORKDIR /app
+ 
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 litencatt litencatt@gmail.com
+
+*/
+package main
+
+import "github.com/litencatt/rlp/cmd"
+
+func main() {
+	cmd.Execute()
+}
