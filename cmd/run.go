64cad5356ebc5c6cb1e54ca7708a9d073176a9fd
中村 光佑 <nakamura.k@pepabo.com>
2024-03-17 21:13:14 +0900

    Evaluate hand

--- cmd/run.go
+++ cmd/run.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/litencatt/rlp/entity"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -23,12 +24,42 @@ var runCmd = &cobra.Command{
 }
 
 func run() error {
-	fmt.Println("start")
+	fmt.Println("[Game start]")
 
 	deck := entity.NewDeck()
-	deck.Shuffle()
-	hand := deck.Deal(8)
-	fmt.Println(hand)
+	//deck.Shuffle()
+
+	// loop
+	for {
+		fmt.Println("\n[Deal 5 cards]")
+		hand := deck.Deal(5)
+		for _, card := range hand {
+			fmt.Printf("%s of %s\n", card.Rank, card.Suit)
+		}
+
+		// Selct play or discard
+		poker := entity.EvaluateHand(hand)
+		fmt.Printf("\nHand: %s\n\n", poker)
+
+		// play again?
+		validate := func(input string) error {
+			if input != "y" && input != "n" {
+				return fmt.Errorf("invalid input(%s)", input)
+			}
+			return nil
+		}
+		p := promptui.Prompt{
+			Label:    "Play again?",
+			Validate: validate,
+		}
+		result, err := p.Run()
+		if err != nil {
+			return err
+		}
+		if result != "y" {
+			break
+		}
+	}
 
 	return nil
 }
