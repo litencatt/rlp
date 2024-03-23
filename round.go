b3c998e68a5e323736e2f83c3a61ecf316800187
中村 光佑 <nakamura.k@pepabo.com>
2024-03-23 21:33:54 +0900

    Rename

--- round.go
+++ round.go
@@ -8,11 +8,11 @@ import (
 )
 
 type PokerRound struct {
-	Deck       entity.Deck
-	TotalScore int
-	Hands      []entity.Trump
-	Remain     []entity.Trump
-	Hand       []entity.Trump
+	Deck          entity.Deck
+	TotalScore    int
+	HandCards     []entity.Trump
+	RemainCards   []entity.Trump
+	SelectedCards []entity.Trump
 }
 
 func NewRound() *PokerRound {
@@ -25,24 +25,24 @@ func NewRound() *PokerRound {
 }
 
 func (p *PokerRound) Start(defaultDeal int) []string {
-	p.Hands = p.Deck.Draw(defaultDeal)
+	p.HandCards = p.Deck.Draw(defaultDeal)
 
 	return p.HandCardString()
 }
 
 func (p *PokerRound) DrawCard(drawNum int) {
-	p.Hands = nil
+	p.HandCards = nil
 
 	drawCards := p.Deck.Draw(drawNum)
-	p.Hands = append(p.Hands, p.Remain...)
-	p.Hands = append(p.Hands, drawCards...)
+	p.HandCards = append(p.HandCards, p.RemainCards...)
+	p.HandCards = append(p.HandCards, drawCards...)
 
-	domain.SortHand(p.Hands)
+	domain.SortHand(p.HandCards)
 }
 
 func (p *PokerRound) HandCardString() []string {
 	var cards []string
-	for _, card := range p.Hands {
+	for _, card := range p.HandCards {
 		cards = append(cards, card.String())
 	}
 	return cards
@@ -50,7 +50,7 @@ func (p *PokerRound) HandCardString() []string {
 
 func (p *PokerRound) RemainCardString() []string {
 	var cards []string
-	for _, card := range p.Remain {
+	for _, card := range p.RemainCards {
 		cards = append(cards, card.String())
 	}
 	return cards
@@ -64,29 +64,29 @@ func (p *PokerRound) SelectCards(cards []string) int {
 		rank := strings.Split(card, " of ")[0]
 		suit := strings.Split(card, " of ")[1]
 		// Find the card from hand
-		for _, c := range p.Hands {
+		for _, c := range p.HandCards {
 			if string(c.Rank) == rank && string(c.Suit) == suit {
 				selectCards = append(selectCards, c)
 				break
 			}
 		}
 	}
-	p.Hand = selectCards
+	p.SelectedCards = selectCards
 
-	// Calc the remain cards
-	var remainCards []entity.Trump
-	for _, card := range p.Hands {
+	// Calc the RemainCards cards
+	var RemainCardsCards []entity.Trump
+	for _, card := range p.HandCards {
 		if !domain.Contains(selectCards, card) {
-			remainCards = append(remainCards, card)
+			RemainCardsCards = append(RemainCardsCards, card)
 		}
 	}
-	p.Remain = remainCards
+	p.RemainCards = RemainCardsCards
 
-	return len(p.Hand)
+	return len(p.SelectedCards)
 }
 
 func (p *PokerRound) PlayHand() (entity.HandType, int) {
-	handType := entity.EvaluateHand(p.Hand)
+	handType := entity.EvaluateHand(p.SelectedCards)
 	score := entity.GetScore(handType)
 	p.TotalScore += score
 
