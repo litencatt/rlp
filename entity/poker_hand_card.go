a4a7af819c1a5bbba8b7c263ede965d077595e8b
中村 光佑 <nakamura.k@pepabo.com>
2024-03-24 12:46:52 +0900

    Not used

--- entity/poker_hand_card.go
+++ entity/poker_hand_card.go
@@ -6,15 +6,6 @@ type PokerHandCard struct {
 	Trumps []Trump
 }
 
-func (p *PokerHandCard) Contains(trump Trump) bool {
-	for _, t := range p.Trumps {
-		if t == trump {
-			return true
-		}
-	}
-	return false
-}
-
 func (p *PokerHandCard) Sort() {
 	sort.Slice(p.Trumps, func(i, j int) bool {
 		return p.Trumps[i].GetSortOrder() < p.Trumps[j].GetSortOrder()
