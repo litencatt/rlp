ef091bde59507573a12311e634280f16b899563e
中村 光佑 <nakamura.k@pepabo.com>
2024-03-24 12:34:23 +0900

    Remove domain

--- entity/trump.go
+++ entity/trump.go
@@ -92,3 +92,12 @@ func (t Trump) GetSortOrder() int {
 	}
 	return 0
 }
+
+func Contains(trumps []Trump, trump Trump) bool {
+	for _, t := range trumps {
+		if t == trump {
+			return true
+		}
+	}
+	return false
+}
